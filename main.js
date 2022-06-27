0fb481d5b69474049ae34eb8212e41f833902d0f
richmondamponsah <97717876+richmondamponsah@users.noreply.github.com>
2022-06-26 21:36:31 -0700

    completed the testmonials, appointment, and footer sections.

--- main.js
+++ main.js
@@ -16,4 +16,49 @@ const closeNav = () =>{
     openNavBtn.style.display = 'inline-block';
 }
 closeNavBtn.addEventListener('click', closeNav);
-// end nav bar open adn close function
+// end nav bar open Nav close function
+
+//close Nav when a menu item is clicked on tablets and mobile devices
+if(window.innerWidth <= 1024) {
+    document.querySelectorAll('#nav_items li a').forEach(navItem => {
+        navItem.addEventListener('click', () => {
+            closeNav();
+        })
+    })
+};
+
+// On scroll, change the navbar style
+//creates a scroll class toggle
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY >  0);
+});
+
+
+
+// Initialize Swiper //
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 3
+  
+    },
+    // when window width is >= 600px
+    600: {
+        slidesPerView: 2
+       
+      },
+      // when window width is >= 320px
+    320: {
+        slidesPerView: 1
+       
+      }
+  }
+});
